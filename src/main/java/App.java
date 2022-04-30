8fc6e6e7b794b0865f7df2ec2d9aeccc942210e7
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-05-01 00:50:46 +0200

    Update method main class App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Witam w systemie rezerwacji hotelu Overlook");
+    System.out.print("Aktualna wersja systemu: ");
+    System.out.println(1);
+    System.out.print("Wersja developerska: ");
+    System.out.println(true);
   }
 }
